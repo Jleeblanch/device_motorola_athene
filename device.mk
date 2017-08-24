3ab15c2d69f004c09574e17fe5ec02bb5a0f44aa
Luca Stefani <luca020400@lineageos.org>
2017-08-24 19:22:46 +0200

    athene: ril: add rild_socket shim
    Change-Id: I095a0f4cd45d6ef63a8a4315723ffe0d7784b708
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- device.mk
+++ device.mk
@@ -267,7 +267,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcnefeatureconfig \
     librmnetctl \
-    libxml2
+    libxml2 \
+    rild_socket
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
