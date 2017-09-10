a883ba0e3b50051373e90b1d96575a422f6c896c
codeworkx <daniel.hillenbrand@codeworkx.de>
2017-09-10 12:29:11 +0200

    athene: seccomp: add mediacodec policy
    Change-Id: I516b6d9de4ed10b96c0a1c09bab263637ed93df8
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- device.mk
+++ device.mk
@@ -265,6 +265,10 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    device/motorola/athene/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
