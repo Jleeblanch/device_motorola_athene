rajatgupta1998/android_device_motorola_athene

athene: provide ASensor_getHandle for libmdmcutback

libshims/sensor.cpp
@@ -1,0 +1,22 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gui/Sensor.h>
+
+extern "C" int ASensor_getHandle(ASensor const* sensor)
+{
+    return static_cast<android::Sensor const*>(sensor)->getHandle();
+}

Jleeblanch/device_motorola_athene

Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

libshims/sensor.cpp
@@ -1,0 +1,22 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <sensor/Sensor.h>
+
+extern "C" int ASensor_getHandle(ASensor const* sensor)
+{
+    return static_cast<android::Sensor const*>(sensor)->getHandle();
+}