a712499b9fede65c2a00eefffa11d5b7c1ea6d00
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2018-06-24 23:40:53 -0400

    athene: Remove overlay-lineage
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- device.mk
+++ device.mk
@@ -22,8 +22,7 @@ $(call inherit-product, vendor/motorola/athene/athene-vendor.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
